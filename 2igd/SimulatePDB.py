2b8b873415af9e4cc30d38c2b1a583a71bdba393
David A Case <david.case@rutgers.edu>
2023-07-18 21:10:33 -0400

    first stab at using mainly openmm commands to get started

---
+++ 2igd/SimulatePDB.py
@@ -0,0 +1,39 @@
+from openmm.app import *
+from openmm import *
+from openmm.unit import *
+from sys import stdout
+import parmed as pmd
+u = pmd.unit
+
+pdb = PDBFile('2igd_solvated.pdb')
+forcefield = ForceField('amber14-all.xml', 'amber14/tip3pfb.xml')
+system = forcefield.createSystem(pdb.topology, nonbondedMethod=PME,
+        nonbondedCutoff=1*nanometer, constraints=HBonds)
+integrator = LangevinMiddleIntegrator(300*kelvin, 1/picosecond, 0.004*picoseconds)
+simulation = Simulation(pdb.topology, system, integrator)
+simulation.context.setPositions(pdb.positions)
+
+e=simulation.context.getState(getEnergy=True).getPotentialEnergy()
+print(' Initial energy = %10.4f kcal/mol' % e.value_in_unit(u.kilocalories_per_mole))
+
+# Now write a serialized state that has coordinates
+print('Writing serialized XML restart file...')
+with open('system2.xml', 'w') as f:
+    f.write(
+            openmm.XmlSerializer.serialize(
+                simulation.context.getState(
+                    getPositions=True, getVelocities=True, getForces=True,
+                    enforcePeriodicBox=system.usesPeriodicBoundaryConditions(),
+                    getEnergy=True
+                )
+            )
+    )
+
+print('Done!')
+quit()
+
+simulation.minimizeEnergy()
+simulation.reporters.append(PDBReporter('output.pdb', 1000))
+simulation.reporters.append(StateDataReporter(stdout, 1000, step=True,
+        potentialEnergy=True, temperature=True))
+simulation.step(10000)
